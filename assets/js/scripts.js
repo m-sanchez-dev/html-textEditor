8bd7a1af75c07ce59f58282fb2d92263d76ba73c
sWallyx <mikelsmartinez@aol.com>
2019-12-22 14:19:33 +0000

    divide into functions script file

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,28 @@
+// Script file, with all the JS code that will make the editor work
+
 // If there's anything in the storage
 if (localStorage.getItem("text_in_editor") !== null) {
   // ...then show it
-  document.getElementById("editor").innerHTML = localStorage.getItem(
-    "text_in_editor"
-  );
+  returnSavedData();
 }
 
 // listen to key presses
 document.addEventListener("keydown", function(e) {
   // once a key is pressed, save whatever's in our box to localstorage
+  saveTextArea();
+});
+
+
+/* Functions to save or gatter the textarea info */
+function saveTextArea() {
   localStorage.setItem(
     "text_in_editor",
     document.getElementById("editor").innerHTML
   );
-});
+}
+
+function returnSavedData() {
+  document.getElementById("editor").innerHTML = localStorage.getItem(
+    "text_in_editor"
+  );
+}
