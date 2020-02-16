c559f2f354d2344216e7d9d96097f32689a8514f
sWallyx <mikelsmartinez@aol.com>
2020-02-16 13:33:08 +0000

    update function position and documentation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,40 +1,52 @@
 // Script file, with all the JS code that will make the editor work
 
-function chekForSpecialCharacter(character) {
-  var endKeys = ["(", "[", "{"];
-  var text = "";
-
-  if (endKeys.includes(character)) {
-    switch (character) {
-      case "(":
-        text = ")";
-        break;
-      case "{":
-        text = "}";
-        break;
-      case "[":
-        text = "]";
-        break;
-    }
-    insertTextAtCursor(text);
-  }
-}
-
-function insertTextAtCursor(text) {
+/**
+ * Adds one character to the next position of the cursor.
+ * @param {string} character - Character to add
+ */
+function insertTextAtCursor(character) {
   var sel, range;
   if (window.getSelection) {
     sel = window.getSelection();
     if (sel.getRangeAt && sel.rangeCount) {
       range = sel.getRangeAt(0);
       range.deleteContents();
-      range.insertNode(document.createTextNode(text));
+      range.insertNode(document.createTextNode(character));
     }
   } else if (document.selection && document.selection.createRange) {
-    document.selection.createRange().text = text;
+    document.selection.createRange().text = character;
   }
 }
 
-/* Functions to save or gatter the textarea info */
+/**
+ * Checks if the character is one of the followings: {},(),[];
+ * In case it is, adds the close character of the one introduced
+ * @param {string} character - Text character
+ */
+function chekForSpecialCharacter(character) {
+  var endKeys = ["(", "[", "{"];
+  var close_character = "";
+
+  if (endKeys.includes(character)) {
+    switch (character) {
+      case "(":
+        close_character = ")";
+        break;
+      case "{":
+        close_character = "}";
+        break;
+      case "[":
+        close_character = "]";
+        break;
+    }
+    insertTextAtCursor(close_character);
+  }
+}
+
+/**
+ * Functions to save or gatter the textarea info.
+ * Saves the data on localStorage.
+ */
 function saveTextArea() {
   localStorage.setItem(
     "text_in_editor",
@@ -42,6 +54,10 @@ function saveTextArea() {
   );
 }
 
+/**
+ * Gets the data from localStorage. And adds it to
+ * the text area.
+ */
 function returnSavedData() {
   // print saved data into textDiv
   document.getElementById("editor").innerHTML = localStorage.getItem(
@@ -59,7 +75,7 @@ if (localStorage.getItem("text_in_editor") !== null) {
 }
 
 // listen to key presses
-document.addEventListener("keydown", function(e) {
+document.addEventListener("keydown", function (e) {
   // checks for special character for autocomplete
   chekForSpecialCharacter(e.key);
 
